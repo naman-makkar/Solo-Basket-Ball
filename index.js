0174f5f10a23d37bd9465268121f852cd233905f
naman-makkar <namanmakkar6@gmail.com>
2023-02-21 20:33:14 +0530

    added start, reset, scoreboard shadow, gif

--- index.js
+++ index.js
@@ -1,42 +1,125 @@
+//Start
+let startbutton = document.getElementById("start common")
+
+
 // Left
 let scoreBoard = document.getElementById("scoreleft")
 let btn1 = document.getElementById("btn1Left")
 
 let score = 0
 
+// right variables
+let scoreBoardRight = document.getElementById("scoreright")
+let btn2 = document.getElementById("btn1Right")
+
+// function work() {
+// 	function add1left() {
+// 		score +=1
+// 		scoreBoard.textContent = score
+// 	}
+
+// 	function add2left() {
+// 		score +=2
+// 		scoreBoard.textContent = score
+// 	}
+
+// 	function add3left() {
+// 		score +=3
+// 		scoreBoard.textContent = score
+// 	}
+
+// 	// Right
+
+// 	function add1right() {
+// 		scoreR +=1
+// 		scoreBoardRight.textContent = scoreR
+// 	}
+
+// 	function add2right() {
+// 		scoreR +=2
+// 		scoreBoardRight.textContent = scoreR
+// 	}
+
+// 	function add3right() {
+// 		scoreR +=3
+// 		scoreBoardRight.textContent = scoreR
+// 	}
+// }
+
+// function start() {
+// 	startbutton.disabled = true
+// 	if (startbutton.disabled == true) {
+// 		work()
+// 	}
+// }
+
+let scoreR = 0
+
+function scoreShadow() {
+	if(score < scoreR){
+        document.getElementById("scoreright").style.boxShadow = "0px 0px 100px blue"
+        document.getElementById("scoreleft").style.boxShadow = "none"
+        document.getElementById("scoreright").style.border = "1px solid lightblue"
+        document.getElementById("scoreleft").style.border = "none"
+    }  
+    else if(scoreR < score){
+        document.getElementById("scoreright").style.boxShadow = "none"
+        document.getElementById("scoreleft").style.boxShadow = "0px 0px 20px lightblue"
+        document.getElementById("scoreright").style.border = "none"
+        document.getElementById("scoreleft").style.border = "1px solid lightblue"
+    }
+    else if(score === scoreR){
+        document.getElementById("scoreright").style.boxShadow = "none"
+        document.getElementById("scoreleft").style.boxShadow = "none"
+        document.getElementById("scoreright").style.border = "none"
+        document.getElementById("scoreleft").style.border = "none"
+    }
+}
+
 function add1left() {
 	score +=1
 	scoreBoard.textContent = score
+	scoreShadow()
 }
 
 function add2left() {
 	score +=2
 	scoreBoard.textContent = score
+	scoreShadow()
 }
 
 function add3left() {
 	score +=3
 	scoreBoard.textContent = score
+	scoreShadow()
 }
 
 // Right
 
-let scoreBoardRight = document.getElementById("scoreright")
-let btn2 = document.getElementById("btn1Right")
-
-let scoreR = 0
-
 function add1right() {
 	scoreR +=1
 	scoreBoardRight.textContent = scoreR
+	scoreShadow()
 }
 
 function add2right() {
 	scoreR +=2
 	scoreBoardRight.textContent = scoreR
+	scoreShadow()
 }
 
 function add3right() {
 	scoreR +=3
 	scoreBoardRight.textContent = scoreR
+	scoreShadow()
+}
+
+// Reset
+let resetButton = document.getElementById("reset common")
+
+function reset() {
+	scoreR=0
+	score=0
+	scoreBoardRight.textContent=scoreR
+	scoreBoard.textContent=score
 }
